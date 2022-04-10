5ce009179c69999d9705ebfe2b750970026dbf9c
Hiago Patrick <hiogaptk@gmail.com>
2022-04-10 19:07:20 -0300

    implementando UseCase e Controller: TurnUserAdmin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,11 +3,18 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        // Complete aqui
+        const { id } = request.params;
+
+        const user_id = id.toString();
+
+        this.turnUserAdminUseCase.execute({ user_id });
+
+        return response.status(200).send();
+    }
 }
 
 export { TurnUserAdminController };
