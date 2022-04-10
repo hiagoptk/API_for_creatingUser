cee74db91f5b0a0a8731d0c370361418eeb440b4
Hiago Patrick <hiogaptk@gmail.com>
2022-04-10 19:17:19 -0300

    Implementando UseCase e Controller ShowUserProfile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,19 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        // Complete aqui
+
+        const { id } = request.params;
+
+        const user_id = id.toString();
+
+        this.showUserProfileUseCase.execute({ user_id });
+
+        return response.status(200).send();
+    }
 }
 
 export { ShowUserProfileController };
