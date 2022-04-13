0a58aaa48868e6152cec444726f7be08ea0baad0
Hiago Patrick <hiogaptk@gmail.com>
2022-04-13 17:15:10 -0300

    Adicionando Swagger para a documentação da API

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swagerFile from "./swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
